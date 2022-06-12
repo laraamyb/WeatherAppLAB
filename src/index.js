afe4cf79856f3f7fd6dc61bba09018ae703ac391
laraamyb <78761210+laraamyb@users.noreply.github.com>
2022-06-12 10:39:45 +0100

    Imported code from codesandbox

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-input");
+  let cityDisplay = document.querySelector("#city-display");
+  cityDisplay.innerHTML = searchInput.value;
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+function showCity(response) {
+  console.log(response.data);
+}
+function displayLocation(position) {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  console.log(lat);
+  console.log(long);
+  position.preventDefault();
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}lon=${long}&appid=177424da3f8dbafeadee840a7b087feb`;
+  axios.get(url).then(showCity);
+}
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(displayLocation);
+}
+
+let button = document.getElementById("#current-location");
+button.addEventListener("click", getCurrentPosition);
 let weekDays = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 function day;
 
