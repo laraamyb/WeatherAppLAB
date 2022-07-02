d041dac1d386bd90446297e2eabb9c1f1321b555
laraamyb <78761210+laraamyb@users.noreply.github.com>
2022-07-02 16:37:28 +0100

    Date moved, css edited

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
-function search(event) {
+function searchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-input");
   let city = searchInput.value;
   let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(cityUrl).then(displaySearchCity)
 }
-function displaySearchCity (response){
-  searchCityDisplay = document.querySelector("#city-display");
-  searchCityDisplay.innerHTML = response.data.name
-}
-function displayCurrentCity(response){
-  let currentCityDisplay = document.querySelector("#city-display");
-  currentCityDisplay.innerHTML = response.data[0].name
- 
-}
-
 function getLocation(city) {
   let lat = city.coords.latitude;
   let long = city.coords.longitude;
@@ -25,19 +15,25 @@ function getLocation(city) {
 function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(getLocation);
 }
-
+function displaySearchCity (response){
+  searchCityDisplay = document.querySelector("#city-display");
+  searchCityDisplay.innerHTML = response.data.name
+}
+function displayCurrentCity(response){
+  let currentCityDisplay = document.querySelector("#city-display");
+  currentCityDisplay.innerHTML = response.data[0].name
+ 
+}
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
-
-let apiKey ="177424da3f8dbafeadee840a7b087feb";
-
+form.addEventListener("submit", searchCity);
 
 let button = document.querySelector("#current-location");
 button.addEventListener("click", getCurrentPosition);
 
+let apiKey ="177424da3f8dbafeadee840a7b087feb";
+
 
 let weekDays = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
 let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-var dt = new Date();
+var dt = new Date()
 document.getElementById('date-time').innerHTML=dt;
