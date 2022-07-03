3dee4c7dccd377494b8ba61b1dd708dc942610ac
laraamyb <78761210+laraamyb@users.noreply.github.com>
2022-07-03 15:33:29 +0100

    Streamlined city search and display

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
+function displayForecast (response){
+  let searchCityDisplay = document.querySelector("#city-display");
+  searchCityDisplay.innerHTML = response.data.name
+let tempElement = document.querySelector("#temp-today");
+.innerHTML = response.data.temp
+let descriptionElement  = document.querySelector("#description");
+let precElement  = document.querySelector("#prec-today");
+let windElement  = document.querySelector("#wind-today");
+
+}
 function searchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-input");
   let city = searchInput.value;
   let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(cityUrl).then(displaySearchCity);
+  axios.get(cityUrl).then(displayForecast);
 }
 function getLocation(city) {
   let lat = city.coords.latitude;
   let long = city.coords.longitude;
   let currentUrl = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=1&appid=${apiKey}&units=metric`;
-  axios.get(currentUrl).then(displayCurrentCity)
+  axios.get(currentUrl).then(getCity);
+ function getCity (response){
+  let city = response.data[0].name;
+  let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(cityUrl).then(displayForecast)
+console.log(cityUrl)}
 }
 function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(getLocation);
-}
-function displaySearchCity (response){
-  searchCityDisplay = document.querySelector("#city-display");
-  searchCityDisplay.innerHTML = response.data.name;
-  displayForecast;
-}
-
-function displayCurrentCity(response){
-  let currentCityDisplay = document.querySelector("#city-display");
-  currentCityDisplay.innerHTML = response.data[0].name;
-  let city = response.data[0].name;
-  let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(cityUrl).then(displayForecast);
-  console.log(cityUrl)
 }
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchCity);
